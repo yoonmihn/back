1d277b0e5f86c1721aea4c23775cf90f1c20c56d
yoonsung <skawk5678@gmail.com>
2020-08-02 15:50:21 +0900

    first commit

---
+++ src/test/java/yoonmihn/onepick/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package yoonmihn.onepick.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
